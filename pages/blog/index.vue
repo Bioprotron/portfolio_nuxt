4cc2e36338ba79ad11f0fb1f350c8362d035efb4
unknown <you@example.com>
2023-12-04 20:19:22 +0300

    portfolio

---
+++ pages/blog/index.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1>hello from the blog</h1>
+    </div>
+</template>
+
+<script setup>
+    const result = await queryContent('/').find()
+
+    console.log(result)
+</script>
+
+<style lang="scss" scoped>
+
+</style>
